cec0d661a487b3f932e7c18dc9857f83acf68ef2
JRybelis <72306551+JRybelis@users.noreply.github.com>
2020-12-03 13:17:21 +0200

    concentrated form validator.js

--- js/components/form-validator/formValidator.js
+++ js/components/form-validator/formValidator.js
@@ -1,47 +1,37 @@
-import { isEmailValid, isNameValid, isTextValid } from "./validationRules.js"
+import { validation } from "./validationRules.js";
 
 function formValidator ( selector ) {
-    const formDOM = document.querySelector( selector );
+    const formDOM = document.querySelector( selector ); /* bendrinis, po apačia specialūs nukreipimai, panaudojus formDOM ir jo (selector) priskyrus konkrečias reikšmes. */
+    
     const submitBtnDOM = formDOM.querySelector("input[type='submit']");
+    if (!submitBtnDOM) {
+        console.error("ERROR: the submit form button was not located.");
+        return false;
+    }
 
     const allInputDOMs = formDOM.querySelectorAll("input:not([type='submit'])");
     const allTextareaDOMs = formDOM.querySelectorAll("textarea");
 
     const allElements = [...allInputDOMs, ...allTextareaDOMs];
-
-    if (!submitBtnDOM) {
-        console.error("ERROR: the submit form button was not located.");
+    if (allElements.length === 0) {
+        console.error ("ERROR: no input or textarea elements were located in the form.");
         return false;
     }
+    
 
     submitBtnDOM.addEventListener("click", () => {
         let errorCount = 0;
         console.clear ();
 
         for (let input of allElements) {
-            const validationRule = input.dataset.validation;
-            const text = input.value;
+            const validationRule = input.dataset.validation; /* all input in *dataset*, marked validation */ 
+            const text = input.value; /* value = vartotojo suteikta medžiaga */
 
-            if (validationRule === 'name') {
-                const nameError = isNameValid(text);
-                if (nameError !== true) {
-                    console.log(nameError);
-                    errorCount ++;
-                }
-            }
-            if (validationRule === 'email') {
-                const emailError = isEmailValid(text);
-                if (emailError !== true) {
-                    console.log(emailError);
-                    errorCount ++;
-                }
-            }
-            if (validationRule === "text") {
-                const textError = isTextValid(text);
-                if (textError !== true) {
-                    console.log(textError);
-                    errorCount ++;
-                }
+            const validationFunction = validation[validationRule];
+            const error = validationFunction(text);
+            if (error !== true) {
+                console.log(error);
+                errorCount++;
             }
         }
         if (errorCount === 0) {
--- js/components/form-validator/validationRules.js
+++ js/components/form-validator/validationRules.js
@@ -2,4 +2,11 @@ import { isEmailValid } from "./isEmailValid.js";
 import { isNameValid } from "./isNameValid.js";
 import { isTextValid } from "./isTextValid.js";
 
-export { isEmailValid, isNameValid, isTextValid }
+// validation object keywords match the terms assigned to the data-validation sections in the form elements, in index.html
+const validation = {
+    email: isEmailValid,
+    text: isTextValid,
+    name: isNameValid
+}
+
+export { validation }
